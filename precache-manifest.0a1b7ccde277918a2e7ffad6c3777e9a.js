9fdcca8e4a67e1c96a0abce79753385e21aa2b0e
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2020-07-16 17:04:26 +0300

    Updates

---
+++ precache-manifest.0a1b7ccde277918a2e7ffad6c3777e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83f8b1d402cc82a89c459af1cc5c8fa",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "f9a4fcf4f8e60001f85e",
+    "url": "/blog-react/static/css/main.701d4be5.chunk.css"
+  },
+  {
+    "revision": "1a2eb00d7e621c548288",
+    "url": "/blog-react/static/js/2.2dde3deb.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/blog-react/static/js/2.2dde3deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a4fcf4f8e60001f85e",
+    "url": "/blog-react/static/js/main.3a1a9562.chunk.js"
+  },
+  {
+    "revision": "0311af46b22740373cfa",
+    "url": "/blog-react/static/js/runtime-main.6305386b.js"
+  }
+]);
